c43057a983d2c62d4d34e8891c77b4da4a3056ee
t-robarge <timothy.robarge@uconn.edu>
2023-03-22 08:24:13 -0400

    new files

---
+++ name_list.py
@@ -0,0 +1,8 @@
+nameset = ['Mike', 'Dave', 'Jimmy', 'Tony', 'Louis', 'Ricardo', 'Alexa', 'Emily', 'Michelle', 'Abby', 'Chloe','Adrian','Matt','Grant','Kaylee','Devin','Erin','Kyle','Ryan','Jimmy','Tony']
+
+hobbys = []
+
+values = []
+
+
+
