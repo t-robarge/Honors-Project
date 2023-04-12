d51e9b8e10bd0a4abc028fb9455ae4b10912adec
t-robarge <timothy.robarge@uconn.edu>
2023-04-12 19:15:03 -0400

    implementing random algorithm

--- name_list.py
+++
@@ -1,8 +0,0 @@
-nameset = ['Mike', 'Dave', 'Jimmy', 'Tony', 'Louis', 'Ricardo', 'Alexa', 'Emily', 'Michelle', 'Abby', 'Chloe','Adrian','Matt','Grant','Kaylee','Devin','Erin','Kyle','Ryan','Jimmy','Tony']
-
-hobbys = []
-
-values = []
-
-
-
